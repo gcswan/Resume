890be6579acff057577accf4a18c14f639dd9520
Greg Swan <greg.c.swan@gmail.com>
2017-07-10 18:17:43 -0500

    got typed.js working

---
+++ script.js
@@ -0,0 +1,9 @@
+
+import Typed from 'type/typed.js';
+
+var options = {
+  strings: ["<i>First</i> sentence.", "Second sentence."],
+  typeSpeed: 40
+}
+
+var typed = new Typed(".element", options);
